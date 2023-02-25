872e19e16f72fa76caff3c82cfa0677128ddc00c
yusufyuhan <115976807+mlyusufyuhan@users.noreply.github.com>
2023-02-25 19:40:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+def run_guess(guess, answer):
+    if  0 < guess < 11:
+        if guess == answer:
+            print('you are a genius!')
+            return True
+    else:
+        print('hey bozo, I said 1~10')
+        return False
+
+if __name__ == '__main__':      # so that the game don't start while testing.
+    answer = random.randint(1, 10)
+    while True:
+        try:
+            guess = int(input('guess a number 1~10:  '))
+            if (run_guess(guess, answer)):
+                break
+        except ValueError:
+            print('please enter a number')
+            continue
